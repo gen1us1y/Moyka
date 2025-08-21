f5f60d9c9ed157118871d30e1679753ddf764fde
gen1us1y <arsigonin01@gmail.com>
2025-08-21 17:09:02 +0700

    3

--- src/contexts/ApiContext.jsx
+++ src/contexts/ApiContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from 'react';
 export const ApiContext = createContext();
 
 export const ApiProvider = ({ children }) => {
-  const API_URL = "http://localhost:5173";
+  const API_URL = "http://localhost:3001";
 
   return (
     <ApiContext.Provider value={{ API_URL }}>
